d4dfded3f5e38dde4f041e3219e138d60e066687
jamesabustan <jamesabustan04@gmail.com>
2020-05-14 09:57:27 +0100

    add migration data

--- server/migrations/20200514094201_recipe.js
+++ server/migrations/20200514094201_recipe.js
@@ -1,8 +1,20 @@
+// Table info
+// id 
+// name : text
+// cook_time : text (optional)
+// level : text (easy, medium, hard)
+// url : text
 
-exports.up = function(knex) {
-  
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('recipe', (table) =>{
+      table.increments();
+      table.text('name').notNullable();
+      table.text('cook_time');
+      table.text('level').notNullable();
+      table.text('url').notNullable();
+  })
 };
 
-exports.down = function(knex) {
-  
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('recipe')
 };
